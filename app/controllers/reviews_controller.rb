3a53d7047611cd750bdce9544839ea1ac04dbd63
Hafid Qarchi <haqarchi@gmail.com>
2022-11-09 14:48:25 +0900

    validation for rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,13 +9,18 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  # def index
-  #   @reviews = Review.all
-  # end
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
 
   private
 
